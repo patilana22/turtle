7e3be3feae4c8f935b536fc0bd6b5732dfadf244
Anand Patil <patilana22@7hills.org>
2018-12-10 12:35:01 -0500

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,17 +1,11 @@
 #!/usr/bin/env python
 #
 
-# @author Brian Arnold
-# @version November, 2018
+# @author Anand Patil
+# @version December 12, 2018
 # @course Programming 1
 # @assign.ment Turtle stuff
-# @descrip.tion 
-# 
-# This is a short reference of how to use some of the simpler turtle graphics
-# functions in four example functions, as well as a list of the turtle functions 
-# available in Python 2.
-#
-
+# @descrip.tion Stuffing of turtles
 
 
 # https://docs.python.org/2/library/turtle.html?highlight=turtle#module-turtle
@@ -126,63 +120,14 @@
 
 
 import turtle
+ 
 
+turtle.setup(720,480)
+wiz = turtle.Turtle()
 
-############# Function definitions go here ###################
-
-#########################################
-def demo1():
-    turtle.setup(720,480)        # make a screen 720px wide, 480px high
-    alex = turtle.Turtle()      # create a turtle named alex
-    alex.forward(150)           # tell alex to move forward by 150 units
-    alex.left(90)               # turn by 90 degrees
-    alex.forward(75)            # move forward 75 units
-    
-    #turtle.done()
-    
-
-
-#########################################
-def fnRelativeMovement():
-    turtle.setup(720,480)
-    t1 = turtle.Turtle()     # create a turtle, call it t1
-
-    for i in range(4):         # create a loop that does something four times
-        t1.forward(50)             # go forward 50 units
-        t1.right(90)             # turn right 90 degrees
-
-    turtle.done()
-
-
-
-#########################################
-def fnAbsoluteMovement():
-    turtle.setup(720,480)
-    t1 = turtle.Turtle()    # create a turtle, call it t1
-
-    t1.setheading(0)        # point the direction to zero degrees (east)
-    t1.pendown()            # put the pen down to draw
-    startX = t1.xcor()        # save current X coordinate
-    startY = t1.ycor()        # save current Y coordinate
-    t1.goto(startX + 100, startY + 50)    # move to a new X,Y coordinate, drawing 
-                                        # along the way since the pen is down
-    t1.penup()                # take pen up, stop drawing
-
-
-    t1.setx(startX)            # Set new X coord (no draw since pen is up)
-    startY = startY + 100    # Calculate and set new Y coord
-    t1.sety(startY)
-    t1.pendown()            # put pen down
-    t1.goto(startX + 100, startY + 50)    # go direction to new X,Y coordinate
-
-    turtle.done()
-
-
-
-######### Function calls go below here ###############
-
-demo1()
-#fnRelativeMovement()
-#fnAbsoluteMovement()
-
-
+while True:
+    wiz.penup()
+    wiz.goto(-250, 200)
+    wiz.pendown()
+    wiz.setheading(252)
+    wiz.forward(20)
